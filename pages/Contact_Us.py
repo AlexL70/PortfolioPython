f2ea7d745374e3b698e772729f655ead9da75d17
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-16 13:26:22 +0300

    219. Webforms

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+class ComponentKeys:
+    EMAIL_FORM = "__email__form__"
+    EMAIL_INPUT = "__email__input__"
+    MESSAGE_BOX = "__message__box__"
+    SUBMIT_BTN = "__submit__btn__"
+
+with st.form(key=ComponentKeys.EMAIL_FORM):
+    user_email = st.text_input("Please, enter your email", placeholder="Your email", key=ComponentKeys.EMAIL_INPUT)
+    message_box = st.text_area("Please, enter your message", placeholder="Your message", key=ComponentKeys.MESSAGE_BOX)
+    submit_button = st.form_submit_button("Submit")
+    if submit_button:
+        print("Submit button pressed")
